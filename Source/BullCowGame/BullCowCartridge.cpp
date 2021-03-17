deba5c6ed051cc7ca64042c97d4d6a1efe1ff71d
DivadTenni <davidctenni@gmail.com>
2021-03-17 17:30:23 +1100

    added very small gameplay loop

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,10 +4,25 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
-	PrintLine("Hang Man: The Beginning!");
+	PrintLine(TEXT("Hang Man: The Beginning!"));
+	PrintLine(TEXT("Please enter your 6 letter guess")); //change the number to be randomly generated
+	TheWord = TEXT("Saturn");
+	TheWord.ToLower();
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
+	ClearScreen();
+	
+	
 
+	FString PlayerGuess = Input;
+	PrintLine(PlayerGuess);
+	if (PlayerGuess == TheWord)
+	{
+		PrintLine(TEXT("You Win!"));
+	} else
+	{
+		PrintLine(TEXT("Guess Again!"));
+	}
 }
