1f6f5c0844137780247d71b7a735318dd1bc1e97
DivadTenni <davidctenni@gmail.com>
2021-03-17 16:34:56 +1100

    Add project files.

---
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#include "BullCowCartridge.h"
+
+void UBullCowCartridge::BeginPlay() // When the game starts
+{
+    Super::BeginPlay();
+	PrintLine("Hang Man: The Beginning");
+}
+
+void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+{
+
+}
